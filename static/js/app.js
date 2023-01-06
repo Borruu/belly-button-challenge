25f90e41c69dab44fc70eb7d0508d78cfc240fb1
Brianna <oconnor.brianna@gmail.com>
2023-01-06 18:25:35 +0800

    access sample dictionary, generate empty bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -20,48 +20,68 @@ dataPromise.then(function (data) {
   }
 });
 
-// create a horizontal bar chart to display the top 10 OTUs found in the individual
+// function to select one dictionary from array in samples
 
-// use sample_values as values for the bar chart (y axis)
-
-// event listener dropdown menu
-function optionChanged() {
-  console.log(this);
-  let subject = d3.select(this.value);
-
+// define function optionChanged
+function optionChanged(subId) {
+  console.log(subId);
+  var findName = subId;
+  // let nameIn = d3.select(this.text);
   dataPromise.then(function (data) {
     let sampleArray = Object.values(data.samples);
-    function findSubject(subject) {
-      return subject.id == this.text;
-    }
-    let sample_dict = sampleArray.filter(findSubject);
+    console.log(sampleArray);
+    let nameIn = d3.select("#selDataset").value;
+    console.log(nameIn);
+    let sample_dict = sampleArray.filter(function findSubject(subject) {
+      // console.log(subject.id == findName);
+      return subject.id == findName;
+    });
     console.log(sample_dict);
+
+    let samId = sample_dict.id;
+    let samV = sample_dict.sample_values;
+    let otuIds = sample_dict.otu_ids;
+    let otuLabels = sample_dict.otu_labels;
+    // use otu_ids as labels (x axis)
+    // use sample_values as values for the bar chart (y axis)
+    // use otu_labels as hovertext for chart
+    var trace1 = {
+      x: otuIds,
+      y: samV,
+      type: "bar",
+      orientation: "h",
+      text: otuLabels,
+    };
+    var data = [trace1];
+    Plotly.newPlot("bar", data);
   });
 }
+// event listener dropdown menu
+let drop = d3.selectAll("#selDataset");
+drop.on("change", optionChanged);
+
+// sample_dict
+
 // let samId = sampleArray.id;
 // let samV = sampleArray.sample_values;
 // let otuIds = sampleArray.otu_ids;
 // let otuLables = sampleArray.otu_labels;
-// use otu_isd as labels (x axis)
 
-// use otu_labels as hovertext for chart
+// function init() {
+//   let myData = dataPromise.then(function (data) {
+//     console.log(data);
+//     return data;
+//   });
 
-function init() {
-  let myData = dataPromise.then(function (data) {
-    console.log(data);
-    return data;
-  });
+//   let sb_names = myData.names;
 
-  let drop = d3.select("#selDataset");
-  let sb_names = myData.names;
+//   for (each in sb_names) {
+//     let thisOp = drop.append("option").text(sb_names[each]);
+//     thisOp.value(parseInt(sb_names[each]));
+//   }
+// }
 
-  for (each in sb_names) {
-    let thisOp = drop.append("option").text(sb_names[each]);
-    thisOp.value(parseInt(sb_names[each]));
-  }
-}
-
-dataPromise.then(init());
+// dataPromise.then(init());
 // Uncaught TypeError: Cannot convert undefined or null to object
 // at Function.values (<anonymous>)
 // at init (app.js:21:25)
